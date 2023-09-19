c94b6df6f940f37fb3695e18de98d16b0303d73e
239yash <239yash@gmail.com>
2023-09-19 18:54:05 +0530

    add: initial commit for book bucket backend app

---
+++ src/test/java/com/example/bookBucketBackend/BookBucketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookBucketBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBucketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
