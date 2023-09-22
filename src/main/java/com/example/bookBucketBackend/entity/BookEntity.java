684bb3c0bea4f0657731a4cead5d19b6aa7a4760
239yash <239yash@gmail.com>
2023-09-22 23:29:56 +0530

    add: getBooksList implemented

--- src/main/java/com/example/bookBucketBackend/entity/BookEntity.java
+++ src/main/java/com/example/bookBucketBackend/entity/BookEntity.java
@@ -1,9 +1,13 @@
 package com.example.bookBucketBackend.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "book")
+@Getter
+@Setter
 public class BookEntity {
     @Id
     private String bookId;
--- src/main/java/com/example/bookBucketBackend/service/books/BookService.java
+++ src/main/java/com/example/bookBucketBackend/service/books/BookService.java
@@ -1,16 +1,30 @@
 package com.example.bookBucketBackend.service.books;
 
 import com.example.bookBucketBackend.dto.response.BookList;
+import com.example.bookBucketBackend.entity.BookEntity;
 import com.example.bookBucketBackend.repository.BookRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class BookService {
     private final BookRepository bookRepository;
     public BookList getBooksList() {
-        bookRepository.addBook();
-        return null;
+        List<BookEntity> data = bookRepository.getAllBooks();
+        List<BookList.Book> books = new ArrayList<>();
+        BookList.Book bookData = new BookList.Book();
+        for (BookEntity book : data) {
+            bookData.setBookName(book.getBookName());
+            bookData.setBookId(book.getBookId());
+            bookData.setYear(book.getYear());
+            bookData.setBranch(book.getBranch());
+            bookData.setCountsAvailable(book.getCountsAvailable());
+            books.add(bookData);
+        }
+        return BookList.builder().books(books).build();
     }
 }
