d5e030064e096bdc00dc5a559c03da5cd3ad35e7
239yash <239yash@gmail.com>
2023-09-23 18:49:31 +0530

    update: addBook repository method added

--- src/main/java/com/example/bookBucketBackend/repository/BookRepository.java
+++ src/main/java/com/example/bookBucketBackend/repository/BookRepository.java
@@ -1,6 +1,5 @@
 package com.example.bookBucketBackend.repository;
 
-import com.example.bookBucketBackend.dto.response.BookList;
 import com.example.bookBucketBackend.entity.BookEntity;
 import lombok.AllArgsConstructor;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -15,11 +14,8 @@ import java.util.List;
 public class BookRepository {
     private final MongoTemplate mongoTemplate;
 
-    public void addBook() {
-        BookList.Book book = new BookList.Book();
-        book.setBookName("xyz");
-
-        mongoTemplate.save(book);
+    public void addBook(BookEntity bookEntity) {
+        mongoTemplate.save(bookEntity);
     }
 
     public List<BookEntity> getAllBooks() {
