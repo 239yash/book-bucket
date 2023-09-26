c19f97d373ee6f2d6e8c81f512ac6b223d6c0f2d
239yash <239yash@gmail.com>
2023-09-26 22:17:54 +0530

    add: setup order repository

---
+++ src/main/java/com/example/bookBucketBackend/repository/OrderRepository.java
@@ -0,0 +1,22 @@
+package com.example.bookBucketBackend.repository;
+
+import com.example.bookBucketBackend.entity.OrderEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class OrderRepository {
+    private final MongoTemplate mongoTemplate;
+
+    public OrderEntity getLiveOrderByUser(String userId) {
+        Criteria criteria = Criteria
+                .where("userId").is(userId)
+                .and("isSubmitted").is(false)
+                .and("isDeleted").is(false);
+        return mongoTemplate.findOne(new Query(criteria), OrderEntity.class);
+    }
+}
