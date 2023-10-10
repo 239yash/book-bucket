bf66dd5f549061f6b3612d8e4d49090d5c6f84af
239yash <239yash@gmail.com>
2023-10-10 23:09:20 +0530

    add: orders listing for admin flow

--- src/main/java/com/example/bookBucketBackend/controller/admin/AdminController.java
+++ src/main/java/com/example/bookBucketBackend/controller/admin/AdminController.java
@@ -1,4 +1,27 @@
 package com.example.bookBucketBackend.controller.admin;
 
+
+import com.example.bookBucketBackend.Constants;
+import com.example.bookBucketBackend.entity.OrderEntity;
+import com.example.bookBucketBackend.service.admin.AdminService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/admin/orders")
+@Slf4j
 public class AdminController {
+    private final AdminService adminService;
+
+    @GetMapping
+    public List<OrderEntity> getAllOrders(@RequestParam Constants.OrderType orderType) {
+        return adminService.getAllOrders(orderType);
+    }
 }
