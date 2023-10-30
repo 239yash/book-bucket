edaf0ede5b6ba7553ad97ca824323531f24beef4
239yash <239yash@gmail.com>
2023-10-30 23:16:22 +0530

    add: models & entity for user auth

---
+++ src/main/java/com/example/bookBucketBackend/entity/UserInfo.java
@@ -0,0 +1,20 @@
+package com.example.bookBucketBackend.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+@Getter
+@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+    @Id
+    private String id;
+    private String name;
+    private String roles;
+    private String email;
+    private String password;
+}
