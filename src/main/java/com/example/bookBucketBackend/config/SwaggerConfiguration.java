484e85f4100e197bd300deb9827dab6b3c3bebf2
239yash <239yash@gmail.com>
2023-12-10 00:44:59 +0530

    update: Swagger bean server config

--- src/main/java/com/example/bookBucketBackend/config/SwaggerConfiguration.java
+++ src/main/java/com/example/bookBucketBackend/config/SwaggerConfiguration.java
@@ -3,13 +3,19 @@ package com.example.bookBucketBackend.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfiguration {
     @Bean
     public OpenAPI myOpenAPI() {
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
+        localServer.setDescription("Server URL in Local environment");
 
         Contact contact = new Contact();
         contact.setName("Yash");
@@ -20,6 +26,6 @@ public class SwaggerConfiguration {
                 .contact(contact)
                 .description("This API exposes endpoints to book-bucket project.");
 
-        return new OpenAPI().info(info);
+        return new OpenAPI().info(info).servers(List.of(localServer));
     }
 }
