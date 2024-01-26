400adea1c6c52329cd0f55a6dbe508b4f57f2384
239yash <239yash@gmail.com>
2024-01-26 22:37:03 +0530

    add: aspect for request-response logging

--- src/main/java/com/example/bookBucketBackend/RequestResponseLogConfiguration.java
+++ src/main/java/com/example/bookBucketBackend/RequestResponseLogConfiguration.java
@@ -1,11 +1,32 @@
 package com.example.bookBucketBackend;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
 @Slf4j
 public class RequestResponseLogConfiguration {
+    @Before("@annotation(org.springframework.web.bind.annotation.PostMapping) " +
+            "|| @annotation(org.springframework.web.bind.annotation.PutMapping)")
+    public void logBeforePostMapping(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            if (arg != null) {
+                if (Arrays
+                        .stream(((MethodSignature) joinPoint.getSignature()).getMethod().getParameterAnnotations())
+                        .flatMap(Arrays::stream)
+                        .anyMatch(annotation -> annotation.annotationType().equals(RequestBody.class))) {
+                    log.info("Request Body(Logged in Aspect) - " + arg);
+                }
+            }
+        }
+    }
 }
