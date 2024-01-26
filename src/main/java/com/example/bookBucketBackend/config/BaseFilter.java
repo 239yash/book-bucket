7ec5c68ebd3bb00871936a512e85227a248cac03
239yash <239yash@gmail.com>
2024-01-26 22:44:52 +0530

    update: removed base filter class (temporary)

--- src/main/java/com/example/bookBucketBackend/config/BaseFilter.java
+++ src/main/java/com/example/bookBucketBackend/config/BaseFilter.java
@@ -1,32 +1,32 @@
-package com.example.bookBucketBackend.config;
-
-import jakarta.servlet.*;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-import java.util.UUID;
-
-@Component
-@Slf4j
-public class BaseFilter implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        MDC.clear();
-        String traceId = UUID.randomUUID().toString();
-        MDC.put("traceId", traceId);
-        filterChain.doFilter(servletRequest, servletResponse);
-        MDC.clear();
-    }
-
-    @Override
-    public void destroy() {
-        Filter.super.destroy();
-    }
-}
+//package com.example.bookBucketBackend.config;
+//
+//import jakarta.servlet.*;
+//import lombok.extern.slf4j.Slf4j;
+//import org.slf4j.MDC;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.IOException;
+//import java.util.UUID;
+//
+//@Component
+//@Slf4j
+//public class BaseFilter implements Filter {
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        Filter.super.init(filterConfig);
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        MDC.clear();
+//        String traceId = UUID.randomUUID().toString();
+//        MDC.put("traceId", traceId);
+//        filterChain.doFilter(servletRequest, servletResponse);
+//        MDC.clear();
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        Filter.super.destroy();
+//    }
+//}
