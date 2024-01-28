ac0591a0f75896b68614d01e13e751431aac5dd8
239yash <239yash@gmail.com>
2024-01-28 23:36:06 +0530

    update: auth refactor for di

--- src/main/java/com/example/bookBucketBackend/authentication/WebSecurityConfig.java
+++ src/main/java/com/example/bookBucketBackend/authentication/WebSecurityConfig.java
@@ -1,7 +1,7 @@
 package com.example.bookBucketBackend.authentication;
 
 import com.example.bookBucketBackend.service.user.UserInfoService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,10 +20,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@AllArgsConstructor
 public class WebSecurityConfig {
-    @Autowired
     private JwtAuthFilter authFilter;
-    @Autowired
     private PasswordEncoder passwordEncoder;
     @Bean
     public UserDetailsService userDetailsService() {
