fc077723abf4015db25befd051de06e8d3b7ea3d
239yash <239yash@gmail.com>
2024-01-28 23:36:23 +0530

    update: filters refactor

--- src/main/java/com/example/bookBucketBackend/config/BaseInterceptor.java
+++ src/main/java/com/example/bookBucketBackend/config/BaseInterceptor.java
@@ -2,6 +2,7 @@ package com.example.bookBucketBackend.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -10,7 +11,9 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Slf4j
 public class BaseInterceptor implements HandlerInterceptor {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(@NonNull HttpServletRequest request,
+                             @NonNull HttpServletResponse response,
+                             @NonNull Object handler) {
         return true;
     }
 }
